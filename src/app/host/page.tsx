5f01dbac4b020c1324f3e7443d663782ecccf573
Rupin <rupinthapa0623@gmail.com>
2025-11-15 17:43:41 +0530

    project setup

---
+++ src/app/host/page.tsx
@@ -0,0 +1,10 @@
+// src/app/host/page.tsx
+export default function HostPage() {
+    return (
+      <main>
+        <h1>Quizmaster Control Panel</h1>
+        <p>Select round, show questions, control timer, etc </p>
+      </main>
+    );
+  }
+  
